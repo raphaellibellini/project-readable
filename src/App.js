a254e42fc5c21feb8f7d73fb8fd306d950c88592
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-05-30 01:17:38 -0300

    Get categories

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import './App.css';
 import { connect } from 'react-redux'
 import { receivePosts } from './actions/posts'
 import * as ReadableAPI from './utils/ReadableAPI'
+import { receiveCategories } from './actions/categories';
 
 class App extends Component {
   componentDidMount() {
     ReadableAPI.getPostsInCategory()
       .then(res => this.props.dispatch(receivePosts(res)))
+
+    ReadableAPI.getCategories()
+      .then(res => this.props.dispatch(receiveCategories(res)))
   }
 
   render() {
+    const { categories } = this.props
     return (
       <div>
+        <div>
+          <nav>
+            <ul>
+              {categories && categories.map((category) => (
+                <li key={category.path}>
+                  <b>{category.name}</b>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        
         {this.props.posts.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
@@ -27,7 +44,8 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
-    posts: state.posts
+    posts: state.posts,
+    categories: state.categories
   }
 }
 
