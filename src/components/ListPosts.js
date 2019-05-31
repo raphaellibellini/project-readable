3da53744a76c27139dff843606f2be557d9d0e30
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-05-31 17:33:41 -0300

    Sort posts by date or vote score

--- src/components/ListPosts.js
+++ src/components/ListPosts.js
@@ -3,6 +3,29 @@ import { connect } from 'react-redux'
 import Post from './Post'
 
 class ListPosts extends Component {
+    state = {
+        sortBy: 'date'
+    }
+    
+    handleChange = e => {
+    const value = e.target.value
+
+        this.setState(() => ({
+            sortBy: value
+        }))
+    }
+    
+    sortBy(posts) {
+        switch (this.state.sortBy) {
+            case 'voteScore':
+                return posts.sort((a, b) => b.voteScore - a.voteScore)
+            case 'date':
+                return posts.sort((a, b) => b.timestamp - a.timestamp)
+            default:
+                return posts
+        }
+    }
+
     render() {
         let { posts } = this.props
         const category = this.props.match.params.id
@@ -12,8 +35,23 @@ class ListPosts extends Component {
             })
         }
 
+        if (posts !== undefined && posts.length > 1) {
+            posts = this.sortBy(posts)
+        }
+
         return(
             <div>
+                <form>
+                    <label>
+                    Sort By
+                        <select onChange={e => this.handleChange(e)}>
+                            <option value="date" defaultValue>Date</option>
+                            <option value="voteScore">Score</option>
+                        </select>
+                    </label>
+                </form>
+
+
                 <ul>
                     {posts.map(post => (
                         <li key={post.id}>
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 
 class Post extends Component {
     render() {
